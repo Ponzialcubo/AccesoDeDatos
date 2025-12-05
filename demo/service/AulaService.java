6e013aa12ab6e8b71135f51e48f8445fc2412fda
cokeIkigai <coke_vader@hotmail.com>
2025-12-05 11:51:11 +0100

    Create AulaService interface for reservation management
    Define the AulaService interface with methods for managing reservations.

---
+++ demo/service/AulaService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Aula;
+import java.util.List;
+
+public interface AulaService {
+
+// Recoger todas las reservas.
+// recoger una reserva.
+// Guardar una reserva --Se debe validar horas
+// Cambiar una reserva.
+// Borrar una reserva.
+}
